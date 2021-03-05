a792fe36c8f78d242646ca3d295bf92ab60f9602
shamprashant <shamprashant2@gmail.com>
2021-03-05 16:13:56 +0530

    all data types

---
+++ json_support.py
@@ -0,0 +1,27 @@
+# Python program showing that 
+# json support different primitive 
+# types 
+  
+import json 
+  
+# list conversion to Array 
+print(json.dumps(['Welcome', "to", "Github"])) 
+  
+# tuple conversion to Array 
+print(json.dumps(("Welcome", "to", "Github"))) 
+  
+# string conversion to String 
+print(json.dumps("Hi")) 
+  
+# int conversion to Number 
+print(json.dumps(123)) 
+  
+# float conversion to Number 
+print(json.dumps(23.572)) 
+  
+# Boolean conversion to their respective values 
+print(json.dumps(True)) 
+print(json.dumps(False)) 
+  
+# None value to null 
+print(json.dumps(None)) 
